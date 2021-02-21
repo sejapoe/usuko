c9a78356fe67bd2ae712a7740144aab48e50b2d3
Fiori <bekaldiev2015@gmail.com>
2021-02-21 12:20:06 +0300

    delete old src

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'УСУКО МБОУ СОШ №28' });
-});
-
-module.exports = router;
